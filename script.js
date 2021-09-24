1bc28c5c13d9505685bf145d95f7f3abf128976b
Viktor <sladkiysous@gmail.com>
2021-09-25 01:24:02 +0300

    first script

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
-alert('Hello');
+const numberOfFilms =+prompt('Сколько фильмов ты посмотрел?');
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:{},
+    privat:false
+};
+const a=prompt('Один из последних просмотренных фильмов'),
+    b=prompt('На сколько ты его оценишь?'),
+    c=prompt('Один из последних просмотренных фильмов'),
+    d=prompt('На сколько ты его оценишь?');
+    personalMovieDB.movies[a]=b;
+    personalMovieDB.movies[c]=d;
+    console.log(personalMovieDB);

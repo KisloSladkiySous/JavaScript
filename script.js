718334163d42888d311c756a9a77fdfe47610b74
Viktor <sladkiysous@gmail.com>
2021-09-26 23:20:49 +0300

    added second task

--- script.js
+++ script.js
@@ -7,10 +7,28 @@ const personalMovieDB={
     genres:{},
     privat:false
 };
-const a=prompt('Один из последних просмотренных фильмов'),
-    b=prompt('На сколько ты его оценишь?'),
-    c=prompt('Один из последних просмотренных фильмов'),
-    d=prompt('На сколько ты его оценишь?');
-    personalMovieDB.movies[a]=b;
-    personalMovieDB.movies[c]=d;
+
+   
+
+    for(let i = 0;i < 2;i++){
+        
+        const a=prompt('Один из последних просмотренных фильмов'),
+        b=prompt('На сколько ты его оценишь?');
+        if (a!= null && b !=null && a!=`` && b!=`` && a.length<50){
+            personalMovieDB.movies[a]=b;
+            console.log(`da`);
+        } else {
+            console.log(`error`);
+            i--;
+        }
+    }
+    switch(personalMovieDB.count){
+        case 10:
+            console.log(`Harosh`);
+            break;
+        case 15:
+            console.log(`SuperHaroosh`);
+            break;
+        default:console.log(`Krasava`);
+    }
     console.log(personalMovieDB);

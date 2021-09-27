2feb5879f8741420e21134f6036efbc7859b4277
Viktor <sladkiysous@gmail.com>
2021-09-27 03:53:34 +0300

    added scss

--- script.js
+++ script.js
@@ -32,3 +32,46 @@ const personalMovieDB={
         default:console.log(`Krasava`);
     }
     console.log(personalMovieDB);
+
+    function first(){
+        setTimeout(function() {
+            console.log(1);
+        },500);
+    }
+
+    function second(){
+        console.log(2);
+    }
+    first();
+    second();
+
+    function learnJavaScript(lang,callback){
+        console.log(`Я учу:${lang}`);
+        callback();
+    }
+    function done () { 
+        console.log(`Я прошел этот урок!`);
+    }
+    learnJavaScript(`JavaScript`,done);
+
+
+    const options = { 
+        name:`Test`,
+        width:1024,
+        height:1024,
+        colors: {border:`black`, bg: `red`}
+    };
+    let counter =0;
+    console.log(options.colors.bg);
+    for (let key  in options){
+        if (typeof(options[key]) === `object`){
+            for(let i in options[key]){
+                console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+                counter++;
+            }
+        } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+        }
+    }
+console.log(counter);
